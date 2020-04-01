a953db641b3b346662e36e9c94a22547b1b02ad5
Daniel Thompson <daniel.thompson5787@outlook.com>
2020-04-01 13:26:46 -0400

    lab08

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> 
+        {
+            new Product { Name = "Football", Description ="Inflatable Leather", Price = 25 },
+            new Product { Name = "Surf board",Description = " Ride Waves", Price = 179 },
+            new Product { Name = "Running shoes",Description = "Size 11 tires for my feet", Price = 95 }
+        }.AsQueryable<Product>();
+    }
+}
